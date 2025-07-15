ff0b6b860ad6f7028d0134d9e6aafa931feb6b86
Tysharay <159968082+Tysharay@users.noreply.github.com>
2025-07-15 19:43:03 -0400

    Initial load

---
+++ js02-2/chapter/js02_txt.js
@@ -0,0 +1,19 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Chapter case
+
+      Fan Trick Fine Art Photography
+      Variables and functions
+      Author: Toya Jackson
+      Date:   7/15/25
+
+      Filename: js02.js
+ */
+// set the form's default values
+function setupForm() {
+      document.getElementById("photoNun").value = 1;
+      document.getElementById("photoHrs").value = 2;
+      document.getElementById("makeBook").checked = false;
+      document.getElementById("photoRights").checked = false;
+      document.getElementById("photoDist").value = 0;
+}
