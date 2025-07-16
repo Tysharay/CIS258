8f253e7fa7ee6a0fe6270343a499156df3ecd970
Tysharay <159968082+Tysharay@users.noreply.github.com>
2025-07-16 12:37:34 -0400

    Add files via upload

--- js02-2/project03/project02-03_txt.js
+++ js02-2/project03/project02-03_txt.js
@@ -3,8 +3,37 @@
       Project 02-03
 
       Application to return the shape of a clicked object
-      Author: 
-      Date:   
+      Author: Toya Jackson
+      Date:   7/16/25
 
       Filename: project02-03.js
  */
+// onmouseover: show feedback message
+document.getElementById("square").onmouseover = function() {
+   document.getElementById("feedback").innerHTML = "You're hovering over the square";
+};
+
+// onmouseout: clear feedback message
+document.getElementById("square").onmouseout = function() {
+   document.getElementById("feedback").innerHTML = "";
+};
+
+// onmouseover: show feedback message
+document.getElementById("circle").onmouseover = function() {
+   document.getElementById("feedback").innerHTML = "You're hovering over the circle";
+};
+
+// onmouseout: clear feedback message
+document.getElementById("circle").onmouseout = function() {
+   document.getElementById("feedback").innerHTML = "";
+};
+
+// onmouseover: show feedback message
+document.getElementById("triangle").onmouseover = function() {
+   document.getElementById("feedback").innerHTML = "You're hovering over the triangle";
+};
+
+// onmouseout: clear feedback message
+document.getElementById("triangle").onmouseout = function() {
+   document.getElementById("feedback").innerHTML = "";
+};
