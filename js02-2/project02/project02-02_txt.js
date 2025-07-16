ff5e6ea330a91d6c54f826c16a07c413f92288df
Tysharay <159968082+Tysharay@users.noreply.github.com>
2025-07-16 12:36:44 -0400

    Add files via upload

--- js02-2/project02/project02-02_txt.js
+++ js02-2/project02/project02-02_txt.js
@@ -3,9 +3,20 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Toya Jackson
+      Date:   7/16/25
 
       Filename: project02-02.js
  */
- 
+ function verifyForm() {
+   let name = document.getElementById("name").value;
+   let email = document.getElementById("email").value;
+   let phone = document.getElementById("phone").value;
+
+   (name && email && phone)
+      ? window.alert("Thank you!")
+      : window.alert("Please fill in all fields");
+}
+
+// Attach click event listener to the element with id "submit"
+document.getElementById("submit").addEventListener("click", verifyForm);
