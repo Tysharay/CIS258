1a48104c0bcbc0a75853953ed2c87d5d6a64c52d
Tysharay <159968082+Tysharay@users.noreply.github.com>
2025-07-16 12:35:31 -0400

    Add files via upload

--- js02-2/project01/project02-01_txt.js
+++ js02-2/project01/project02-01_txt.js
@@ -3,9 +3,33 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Toya Jackson
+      Date:   7/16/25
 
       Filename: project02-01.js
  */
+// Function to convert Fahrenheit to Celsius
+function FahrenheitToCelsius(degree) {
+   return (degree - 32) / 1.8;
+}
+
+// Function to convert Celsius to Fahrenheit
+function CelsiusToFahrenheit(degree) {
+   return (degree * 1.8) + 32;
+}
+
+// Add onchange event handler to element with id "cValue"
+document.getElementById("cValue").onchange = function() {
+   var cDegree = document.getElementById("cValue").value;
+   document.getElementById("fValue").value = CelsiusToFahrenheit(cDegree);
+};
+
+// Add onchange event handler to element with id "fValue"
+document.getElementById("fValue").onchange = function() {
+   var fDegree = document.getElementById("fValue").value;
+   document.getElementById("cValue").value = FahrenheitToCelsius(fDegree);
+};
+
+
+
 
